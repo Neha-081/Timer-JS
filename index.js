c960cb2a1937a5bc331f93958a84eae0eecf3b0e
Neha <nehaprajapati1150@gmail.com>
2021-12-10 21:22:00 +0530

    timer

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-class Timer{
-    constructor(durationInput,startButton,pauseButton){
-        this.durationInput=durationInput;
-        this.startButton=startButton;
-        this.pauseButton=pauseButton;
-
-        this.startButton.addEventListener('click',this.start)
-    }
-    start=()=>{
-console.log('start');
-    }
-}
-
-
 const durationInput=document.querySelector("#duration")
 const startButton = document.querySelector("#start")
 const pauseButton = document.querySelector("#pause")
 
-const timer = new Timer(durationInput,startButton,pauseButton)
+const timer = new Timer(durationInput,startButton,pauseButton,{
+    onStart(){
+     console.log('timer started');
+    },
+    onTick(){
+        console.log('tciked down');
+    },
+    onComplete(){
+console.log('timer completed');
+    }
+})
---
+++ timer.js
@@ -0,0 +1,47 @@
+class Timer{
+    constructor(durationInput,startButton,pauseButton,callbacks){
+        this.durationInput=durationInput;
+        this.startButton=startButton;
+        this.pauseButton=pauseButton;
+if(callbacks){
+    this.onStart=callbacks.onStart;
+    this.onTick=callbacks.onTick;
+    this.onComplete=callbacks.onComplete;
+}
+        this.startButton.addEventListener('click',this.start)
+        this.pauseButton.addEventListener('click',this.pause)
+    }
+    start=()=>{
+        if(this.onStart){
+            this.onStart()
+        }
+        this.tick()    //to start instantly
+        this.interval=setInterval(this.tick,1000);
+    }
+    pause=()=>{
+        clearInterval(this.interval)
+    }
+    tick=()=>{
+        if(this.timeRemaining<=0){
+            this.pause();
+            if(this.onComplete){
+                this.onComplete()
+            }
+        }else{
+            this.timeRemaining = this.timeRemaining-1
+            //this will set the time       this will get the time
+            if(this.onTick){
+                this.onTick()
+            }
+        }
+
+    }
+    get timeRemaining(){
+        return parseFloat(this.durationInput.value)    //parseFloat will give no with decimal whereas parseInt give no with no decimal
+    
+    }
+    set timeRemaining(time){
+      this.durationInput.value=time;
+    }
+  
+}
